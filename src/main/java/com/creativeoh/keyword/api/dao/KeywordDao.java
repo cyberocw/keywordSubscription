b906f3e09b8c51fed601e834cc7124077bc76cda
cyberocw <cyberocw@gmail.com>
2017-07-05 15:36:51 +0900

    프로퍼티 변경 및 쿼리 group 절 수정

--- src/main/java/com/creativeoh/keyword/api/dao/KeywordDao.java
+++ src/main/java/com/creativeoh/keyword/api/dao/KeywordDao.java
@@ -9,12 +9,10 @@ import org.springframework.data.repository.query.Param;
 import com.creativeoh.keyword.Keyword;
 
 public interface KeywordDao extends JpaRepository<Keyword, Long> {
-	//List<KeywordVO> findAllKeywrod
 	
-	@Query("SELECT k.keyword, sum(k.point) as point, fromSite FROM Keyword k where k.point > 0 and k.typeCode = :typeCode and k.simpleDate >= :simpleDate Group by k.keyword order by point desc")
+	@Query("SELECT k.keyword, sum(k.point) as point, k.fromSite FROM Keyword k where k.point > 0 and k.typeCode = :typeCode and k.simpleDate >= :simpleDate Group by k.keyword, k.fromSite order by point desc")
 	List<Keyword> findBySimpleDateGroupGreaterThanEqual(@Param("typeCode") int typeCode, @Param("simpleDate") long simpleDate);
 	
-	//시점 가져오기 (그룹은 X)
 	List<Keyword> findByTypeCodeAndSimpleDate(int typeCode, long today);
 	
 	Keyword findTopByTypeCodeOrderBySimpleDateDesc(int sum);
