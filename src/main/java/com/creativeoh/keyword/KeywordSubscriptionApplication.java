aca13b95977bc9802941c35943ba847bc5f50cae
cyberocw <cyberocw@gmail.com>
2017-07-07 23:42:23 +0900

    중복 실행 버그 수정

--- src/main/java/com/creativeoh/keyword/KeywordSubscriptionApplication.java
+++ src/main/java/com/creativeoh/keyword/KeywordSubscriptionApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class KeywordSubscriptionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("KeywordSubscriptionApplication void main start");
 		SpringApplication.run(KeywordSubscriptionApplication.class, args);
 	}
 }
